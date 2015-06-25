259c3f845e834308663e291096b2854e137ec6b0
Konstantyn Byhkalo <your-email@whatever.com>
2015-06-25 15:35:02 +0300

    Initial Commit

---
+++ Tests/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tests
+//
+//  Created by Byhkalo Konstantyn on 25.06.15.
+//  Copyright (c) 2015 Byhkalo Konstantyn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tests/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  Tests
+//
+//  Created by Byhkalo Konstantyn on 25.06.15.
+//  Copyright (c) 2015 Byhkalo Konstantyn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
